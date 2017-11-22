78dfa6f43af4428c2cf075e739c06a1b79c058b3
전형탁 <andy395023@gmail.com>
2017-11-22 10:54:21 +0900

    Update Placeholder.py

--- Placeholder.py
+++ Placeholder.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 
 input_data = [1,2,3,4,5]
-x = tf.placeholder(dtype=tf.float32)
-y = x*2
+x = tf.placeholder(dtype=tf.float32) # 플레이스홀더 선언
+y = x*2 # 그래프 정의
 
 sess = tf.Session()
-result = sess.run(y,feed_dict={x:input_data})
+result = sess.run(y,feed_dict={x:input_data}) # 플레이스홀더 x 를 통해 input_data 전달
 
 print(result)
